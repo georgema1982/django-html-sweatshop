57271c8d586a88be297fcc2fcd5d3b66984942f7
George Ma <george.ma1982@gmail.com>
2016-06-15 18:07:00 -0600

    Added some descriptions to the demos

--- example/tables.py
+++ example/tables.py
@@ -2,7 +2,7 @@ from django_tables2.tables import Table
 from django_tables2.columns.base import Column
 
 class DemoTable(Table):
-    id = Column(orderable = False)
+    id = Column()
     
     class Meta:
         attrs = {'class': 'paleblue'}
